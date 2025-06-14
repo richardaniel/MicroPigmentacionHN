d72c1b830988757f8c5d0204fabc2000f1a6d233
richardaniel <richarlopez102002@gmail.com>
2025-06-14 10:10:37 -0600

    Cambios varios

--- src/app/feautures/testimonials/views/testimonials-detail/testimonials-detail.component.ts
+++ src/app/feautures/testimonials/views/testimonials-detail/testimonials-detail.component.ts
@@ -13,19 +13,19 @@ export class TestimonialsDetailComponent implements OnInit {
     {
       name: 'Aryany Ayala',
       text: 'Me encantó la atención increíble , son unos artistas , te evalúan todo desde que tipo de piel edad , toman su tiempo para hacerte la evaluación , 100 recomendando mis cejitas están perfectas.',
-      image: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=200&auto=format&fit=crop',
+      image: '',
       position: ''
     },
     {
       name: 'EL Vis',
       text: 'Esto es una manera de poder sentirse en mas confianza contigo mismo y tu apariencia física. Una única experiencia en Micropigmentación Honduras, La recomiendo al 1,000 por 1,000. Muy satisfecho por el trabajo elaborado por el profesional quien ejecuto mi procedimiento en micropigmentación.',
-      image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=200&auto=format&fit=crop',
+      image: '',
       position: ''
     },
     {
       name: 'Estrellita Ramirez',
       text: 'Hacen excelente trabajo profesional, ético.. muy buen trato Asia los pacientes .hoy me fui aser mi retoque y quede encantada. Nilo piensen para ir y hacerse unas cejas bien hechas con profesionales  gracias.',
-      image: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=200&auto=format&fit=crop',
+      image: '',
       position: ''
     },
     {
@@ -33,7 +33,7 @@ export class TestimonialsDetailComponent implements OnInit {
       text: `Hoy fui atendida por la mejor en Micropigmentación Honduras, y me atrevo a hacerlo público porque 1. Soy exigente y conozco la calidad.
              Gracias primero por cambiar completamente mi rostro su buen trabajo se nota, y segundo gracias por explicar y aclarar todas mis dudas, gracias por la confianza que transmiten. De verdad déjeme felicitarlos por ser los mejores.
             Sobre todo supo tomar su tiempo para que yo saliera satisfecha`,
-      image: 'https://images.unsplash.com/photo-1554151228-14d9def656e4?w=200&auto=format&fit=crop',
+      image: '',
       position: ''
     },
    
